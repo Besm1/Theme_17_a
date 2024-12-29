7b0c1b8b209b8ab083e7c4503401852fb74694ed
besm1 <besn1@yandex.ru>
2024-12-29 21:54:57 +0300

    After alembic

---
+++ app/schemas.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel
+
+class CreateUser(BaseModel):
+    username:str
+    firstname:str
+    lastname:str
+    age:int
+
+    class Config:
+        orm_mode = True
+
+class UpdateUser(BaseModel):
+    firstname:str
+    lastname:str
+    age:int
+
+    class Config:
+        orm_mode = True
+
+class CreateTask(BaseModel):
+    title:str
+    content:str
+    priority:int
+
+    class Config:
+        orm_mode = True
+
+class UpdateTask(BaseModel):
+    title: str
+    content: str
+    priority: int
+
+    class Config:
+        orm_mode = True
